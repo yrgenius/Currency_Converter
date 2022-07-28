c7716f7b7a3adcb097cbf1a85a3095093416b194
Oblivion <yrgenius80@gmail.com>
2022-07-28 16:24:32 +0900

    start dev branch

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Header.css";
+
+export default class Header extends React.Component {
+    render() {
+        return <div>Header</div>;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./components/app/App";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
+        <Header />
         <App />
+        <Footer />
     </React.StrictMode>
 );
