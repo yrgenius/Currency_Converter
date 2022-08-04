b2ed62f6b989b7288f0a30a4a8fc896968b7f591
Anderson <yrgenius80@gmail.com>
2022-08-04 23:08:51 +0300

    added USD out

--- src/components/rate/Rate.jsx
+++ src/components/rate/Rate.jsx
@@ -18,23 +18,24 @@ export default class Rate extends React.Component {
     getRate(){
         const apiKey = '7955d9e713214a36863e9d6c2189a041';
         const url = 'https://openexchangerates.org/api/latest.json?app_id=';
-        const base = `&base=${this.base}`;
-        const country = `&symbols=${this.currency}`;
+        // const base = `&base=${this.base}`;
+        // const country = `&symbols=${this.currency}`;
         let now = '';
 
-        fetch(url + apiKey + base + country)
-            .then(data => {
-                return data.json();
-            })
+        fetch(url + apiKey)
+            .then(data => (data.json()))
             .then(data => {
                 console.log(data);
                 now = new Date(data.timestamp); 
-                console.log(now);
                 
                 this.setState({
                     date: now.getDate(),
                     month: now.getMonth(),
-                    year: now.getFullYear()
+                    year: now.getFullYear(),
+                    currencyRate: {
+                        rub: (data.rates['RUB']).toFixed(2),
+                        eur: (data.rates['EUR']).toFixed(2),
+                    },
                 });
             }
             );
@@ -45,12 +46,12 @@ export default class Rate extends React.Component {
                 <h2 className="rate__title">{`Курсы валют на ${this.state.date} ${this.state.month} ${this.state.year}`}</h2>
                 <div className="wrapper">
                     <div className="rate__item">
-                        <div className="rate__item-title">USD</div>
-                        <div className="rate__item-text">1500 кг</div>
-                        <div className="rate__item-text">3000 kg</div>
+                        <p className="rate__item-title">USD</p>
+                        <p className="rate__item-text">{`${this.state.currencyRate.rub} RUB`}</p>
+                        <p className="rate__item-text">{`${this.state.currencyRate.eur} EUR`}</p>
                     </div>
                     <div className="rate__item">
-                        <div className="rate__item-title">USD</div>
+                        <div className="rate__item-title">BTC</div>
                         <div className="rate__item-text">1500 кг</div>
                         <div className="rate__item-text">3000 kg</div>
                     </div>
