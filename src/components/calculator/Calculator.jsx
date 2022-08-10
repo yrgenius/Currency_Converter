dc6a4bd38832cd3452e6cf5c1e34989a22f3e299
Oblivion <yrgenius80@gmail.com>
2022-08-10 16:03:27 +0900

    change calculator

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -8,13 +8,15 @@ export default class Calculator extends React.Component {
                 <h2 className="calc__title">Калькулятор обмена</h2>
                 <div className="calc__wrapper">
                     <p className="calc__subtitle">USD</p>
-                    <ul>
+                    <ul className="calc__choice">
                         <li className="calc__item">
                             <label htmlFor="">
                                 <input
+                                    className="calc__radio-input"
                                     type="radio"
                                     name="radio"
                                     defaultValue={0}
+                                    checked
                                 />{" "}
                                 Купить
                             </label>
@@ -22,6 +24,7 @@ export default class Calculator extends React.Component {
                         <li className="calc__item">
                             <label htmlFor="">
                                 <input
+                                    className="calc__radio-input"
                                     type="radio"
                                     name="radio"
                                     defaultValue={1}
@@ -30,6 +33,24 @@ export default class Calculator extends React.Component {
                             </label>
                         </li>
                     </ul>
+                    <div className="calc__number">
+                        <input type="number" defaultValue={100} />
+                        <select name="" id="">
+                            <option value="USD">USD</option>
+                            <option value="RUB">RUB</option>
+                            <option value="BTC">BTC</option>
+                            <option value="EUR">EUR</option>
+                        </select>
+                    </div>
+                    <div className="calc__out">
+                        <h4>Результат</h4>
+                        <ul>
+                            <li>EUR 150</li>
+                            <li>EUR 150</li>
+                            <li>EUR 150</li>
+                            <li>EUR 150</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         );
